1354d237dc7322ca1b4fedd9c63987deca5d5021
Sundeep Singh <sundeepdev@gmail.com>
2022-03-29 22:53:19 +0530

    Updated code compatible with Xcode 12

--- cardsUITests/cardsUITests.swift
+++ cardsUITests/cardsUITests.swift
@@ -2,12 +2,12 @@
 //  cardsUITests.swift
 //  cardsUITests
 //
-//  Created by Sundeep Singh on 18/03/22.
+//  Created by Sundeep Singh on 28/03/22.
 //
 
 import XCTest
 
-class CardsUITests: XCTestCase {
+class cardsUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -30,9 +30,18 @@ class CardsUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testHomeLandingPage() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
